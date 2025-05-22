cf68326e220ade7767a325edbc4038d67fc85f35
Aline Bornschein <aline.bornschein@gmail.com>
2025-05-22 18:43:17 +0700

    Update Footer: replace TechNova with Lucid Code Labs logo, update contact info and social icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 import styled from 'styled-components';
+import Image from 'next/image';
 
 const FooterSection = styled.footer`
   background-color: var(--dark);
@@ -35,6 +36,8 @@ const Logo = styled.div`
   font-size: 1.5rem;
   font-weight: 700;
   margin-bottom: 1.25rem;
+  display: flex;
+  align-items: center;
 `;
 
 const Description = styled.p`
@@ -141,16 +144,19 @@ const Footer = () => {
       <Container>
         <FooterGrid>
           <FooterInfo>
-            <Logo>TechNova</Logo>
+            <Logo>
+              <Image 
+                src="/images/logo1.png" 
+                alt="Lucid Code Labs" 
+                width={150} 
+                height={50} 
+                style={{ objectFit: 'contain' }}
+              />
+            </Logo>
             <Description>
               We are a team of passionate developers, designers, and strategists dedicated to creating exceptional digital experiences that drive business growth.
             </Description>
             <SocialLinks>
-              <SocialLink href="#" aria-label="Facebook">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                </svg>
-              </SocialLink>
               <SocialLink href="#" aria-label="Twitter">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
@@ -161,13 +167,6 @@ const Footer = () => {
                   <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                   <rect x="2" y="9" width="4" height="12"></rect>
                   <circle cx="4" cy="4" r="2"></circle>
-                </svg>
-              </SocialLink>
-              <SocialLink href="#" aria-label="Instagram">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-                  <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                 </svg>
               </SocialLink>
             </SocialLinks>
@@ -222,13 +221,10 @@ const Footer = () => {
                 <Link to="contact" smooth={true} duration={500}>Get In Touch</Link>
               </FooterLink>
               <FooterLink>
-                <a href="tel:+15551234567">+1 (555) 123-4567</a>
+                <a href="tel:+4917681417544">+49 176 8141 7544</a>
               </FooterLink>
               <FooterLink>
-                <a href="mailto:hello@technova.com">hello@technova.com</a>
-              </FooterLink>
-              <FooterLink>
-                <a href="#">123 Innovation Way, Tech City, TC 10010</a>
+                <a href="mailto:info@lucidcodelabs.com">info@lucidcodelabs.com</a>
               </FooterLink>
             </FooterLinks>
           </FooterColumn>
@@ -238,7 +234,7 @@ const Footer = () => {
         
         <FooterBottomGrid>
           <Copyright>
-            © {currentYear} TechNova. All rights reserved.
+            © {currentYear} Lucid Code Labs. All rights reserved.
           </Copyright>
           
           <BottomLinks>
